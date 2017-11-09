5253ae34b486c8590988e950070a02bd26bc179a
wcc-mbp <wcc-mbp@wcc-mbpdeMBP.lan>
2017-11-09 20:25:02 +0800

    edit: 将每半秒获取一次EMG的实现改成每一秒获取120长度的数组

--- src/main/java/com/ese/cloud/client/controller/core/MonitorController.java
+++ src/main/java/com/ese/cloud/client/controller/core/MonitorController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -22,8 +25,8 @@ public class MonitorController {
     Logger logger = Logger.getLogger(MonitorController.class);
 
 
-    @RequestMapping(value="/index",method= RequestMethod.GET)
-    public String index(){
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
         return "monitor/index";
     }
 
@@ -33,11 +36,39 @@ public class MonitorController {
     @RequestMapping(value = "/getCyborgEMG", method = RequestMethod.POST)
     @ResponseBody
     public String getCyborgEMG() {
+
+        final class EMGInfo {
+            long timestamp;
+            double value;
+
+            public long getTimestamp() {
+                return timestamp;
+            }
+
+            public void setTimestamp(long timestamp) {
+                this.timestamp = timestamp;
+            }
+
+            public double getValue() {
+                return value;
+            }
+
+            public void setValue(double value) {
+                this.value = value;
+            }
+        }
         try {
-            double fakeEMG = Math.random() * 70;
-            MetricsInfo emgInfo = new MetricsInfo();
-            emgInfo.setValue(Double.toString(fakeEMG));
-            return ReturnData.result(0, "获取申请统计数据成功", JSON.toJSONString(emgInfo));
+            List<EMGInfo> emgInfos = new ArrayList<>();
+            long timestamp = new Date().getTime();
+            for(int i = 0; i < 120; i++) {
+                double fakeEMG = Math.random() * 70;
+                EMGInfo emgInfo = new EMGInfo();
+                emgInfo.setTimestamp(timestamp);
+                emgInfo.setValue(fakeEMG);
+                emgInfos.add(emgInfo);
+                timestamp += 500;
+            }
+            return ReturnData.result(0, "获取申请统计数据成功", JSON.toJSONString(emgInfos));
         } catch (Exception e) {
             logger.error("获取申请统计数据失败：", e);
             return ReturnData.result(-1, "获取申请统计数据失败", null);
--- src/main/resources/static/myjs/monitor/monitor.js
+++ src/main/resources/static/myjs/monitor/monitor.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
 
-    var emgArray = [], now = 0, updateInterval = 500;
+    var emgArray = [], ajaxInterval = 60 * 1000, updateInterval = 500;
 
+    /**
+     * 由于后台数据是假的 所以定时器请求可能得到比正在绘制的更旧的时间戳
+     * 暂时丑陋地加上了一个正在绘制的状态
+     * 目前会导致最多慢一分钟的更新
+     * TODO 等待数据库建立后 增加数据已经获取的状态 就不怕重新获取了
+     * @type {boolean}
+     */
+    var drawing = false;
     /**
      *实时获取电子人肌电信号数据
      */
@@ -17,10 +25,8 @@ $(document).ready(function () {
             },
             success: function (e) {
                 if (e.code == 0) {
-                    var obj = JSON.parse(e.data);
-                    var emgValue = obj.value;
-                    update(emgValue);
-                    console.log(emgValue);
+                    var emgPacket = JSON.parse(e.data);
+                    refreshLoop(update, emgPacket, emgPacket.length);
                 } else {
                     $.gritter.add({
                         title: '执行失败',
@@ -33,14 +39,44 @@ $(document).ready(function () {
     }
 
     /**
+     * 将封装的实时数据数组按照时间戳定时输出
+     * @param func
+     * @param array
+     * @param loopTimes
+     */
+    function refreshLoop(func, array, loopTimes) {
+        var index = 0;
+        var interv = function () {
+            drawing = true;
+            console.log(array[index].timestamp);
+            func(array[index]);
+            var now = array[index].timestamp;
+            index++;
+            if(index >= loopTimes) {
+                drawing = false;
+                return;
+            }
+            setTimeout(interv, array[index].timestamp - now);
+        };
+        var now = new Date().getTime();
+        var startTime = array[0].timestamp;
+        if(startTime > now) {
+            setTimeout(interv, startTime - now);
+        } else {
+            setTimeout(interv, 0);
+        }
+    }
+
+
+
+    /**
      * 根据获取得到的肌电信号的值重绘实时图
      * @param value
      */
     function update(value) {
-        if (emgArray.length > 200)
+        if (emgArray.length > 120)
             emgArray = emgArray.slice(1);
-        now += 1;
-        emgArray.push([now, value]);
+        emgArray.push([value.timestamp, value.value]);
         console.log("update " + value);
         $.plot($("#chartEMG"), [{
             data: emgArray,
@@ -79,8 +115,26 @@ $(document).ready(function () {
             },
             colors: ["#B450B2", "#4A8CF7", "#52e136"],
             xaxis: {
-                ticks: 11,
-                tickDecimals: 0
+                mode: "time",
+                tickSize: [1, "second"],
+                tickFormatter: function (v, axis) {
+                    var date = new Date(v);
+
+                    if (date.getSeconds() % 5 == 0) {
+                        var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+                        var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+                        var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+                        return hours + ":" + minutes + ":" + seconds;
+                    } else {
+                        return "";
+                    }
+                },
+                axisLabel: "Time",
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Verdana, Arial',
+                axisLabelPadding: 10
             },
             yaxis: {
                 ticks: 5,
@@ -92,9 +146,12 @@ $(document).ready(function () {
     /**
      * 设置定时器，每隔一定时间发送异步请求
      */
+    getCyborgEMG();
     setInterval(function () {
-        getCyborgEMG()
-    }, updateInterval);
+        if(!drawing) {
+            getCyborgEMG()
+        }
+    }, ajaxInterval);
 
 });
 
